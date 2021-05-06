0c5e91619c19795a776d027c10d8f80711b6d516
windyakin <windyakin@pepabo.com>
2021-05-06 11:12:21 +0900

    :sparkles: 始まりはみんなの空

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,29 @@
 <template>
   <div :class="$style.container">
     <div>
-      <main>
+      <main class="my-5">
         <h1 :class="$style.title">Liel.la!</h1>
         <hr />
         <h2 :class="$style.subtitle">
           私を<span :class="$style.subtitlePoint">叶</span>える物語
         </h2>
       </main>
-      <aside>
+      <aside class="my-3">
+        <b-button
+          variant="primary"
+          size="sm"
+          :class="$style.btnTwitter"
+          href="https://twitter.com/hashtag/%E5%A7%8B%E3%81%BE%E3%82%8A%E3%81%AF%E3%81%BF%E3%82%93%E3%81%AA%E3%81%AE%E7%A9%BA"
+        >
+          <img src="~/assets/images/twitter.svg" :class="$style.twitterIcon" />
+          #始まりはみんなの空
+        </b-button>
+      </aside>
+      <aside class="my-3" :class="$style.links">
         <b-button
           variant="primary"
           :class="$style.btnTwitter"
-          href="https://twitter.com/intent/tweet?url=https://liel.la/&text=%E7%A7%81%E3%82%92%E5%8F%B6%E3%81%88%E3%82%8B%E7%89%A9%E8%AA%9E"
+          href="https://twitter.com/intent/tweet?url=https://liel.la/&text=%E7%A7%81%E3%82%92%E5%8F%B6%E3%81%88%E3%82%8B%E7%89%A9%E8%AA%9E&hashtag=始まりはみんなの空"
         >
           <img src="~/assets/images/twitter.svg" :class="$style.twitterIcon" />
           ツイート
@@ -43,10 +54,6 @@ export default Vue.extend({})
   text-align: center;
 }
 
-main {
-  margin-bottom: 2rem;
-}
-
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -67,7 +74,7 @@ main {
   color: #e4007f;
 }
 
-aside {
+.links {
   display: inline-flex;
   gap: 0 1rem;
 }
@@ -79,9 +86,8 @@ aside {
 }
 
 .twitterIcon {
-  width: 1.5rem;
-  height: 1.5rem;
+  width: 1.5em;
+  height: 1.5em;
   margin-right: 0.2rem;
 }
-
 </style>
