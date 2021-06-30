a9f46e61276cc63aefac9cfc8f934add2d2cffa6
windyakin <windyakin@users.noreply.github.com>
2021-06-30 00:07:54 +0000

    deploy: efeb597999c853bee31fc3e2b6fe58a22d3cb00d

--- _nuxt/static/1624181663/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624181663",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3654b2c1"}}}}(null,"\u002F"));
